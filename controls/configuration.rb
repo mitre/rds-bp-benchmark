7ee0a303a61746825018f3f95b376385afdd157a
Aaron Lippold <lippold@gmail.com>
2021-05-18 18:34:33 +0000

    - updated controls to use inspec best practices - updated the README to be more complete for AWS profiles - simplified the use of inputs - updated the inspec.yml to be more standardized - removed unused comments
    Signed-off-by: Aaron Lippold <lippold@gmail.com>

--- controls/configuration.rb
+++ controls/configuration.rb
@@ -1,74 +1,81 @@
-title 'RDS - Best Practise - Configuration'
-
-## inputs
-rds_name = input('rds_name')
-rds_publicly_accessible = input('rds_publicly_accessible')
-rds_engine = input('rds_engine')
-rds_deletion_protection = input('rds_deletion_protection')
-rds_auto_minor_version_upgrade = input('rds_auto_minor_version_upgrade')
-rds_iam_database_authentication_enabled = input('rds_iam_database_authentication_enabled')
-rds_performance_insights_enabled = input('rds_performance_insights_enabled')
+title 'RDS - Best Practice - Configuration'
 
 control 'instance' do
-  impact 1.0
+  impact 0.5
+  tag nist: ['CM-6']
+  tag severity: 'medium'
   title 'instance exists'
-  describe aws_rds_instance(rds_name) do
+  describe aws_rds_instance(input('rds_name')) do
     it { should exist }
   end
 end
 
 control 'public' do
-  impact 1.0
+  impact 0.5
+  tag nist: ['CM-6']
+  tag severity: 'medium'
   title 'is instance publicly accessable'
-  describe aws_rds_instance(rds_name) do
-    its('publicly_accessible') { should eq rds_publicly_accessible }
+  describe aws_rds_instance(input('rds_name')) do
+    its('publicly_accessible') { should eq input('rds_publicly_accessible') }
   end
 end
 
 control 'rdms' do
-  impact 1.0
+  impact 0.5
+  tag nist: ['CM-6']
+  tag severity: 'medium'
   title 'engine type'
-  describe aws_rds_instance(rds_name) do
-    its('engine') { should eq rds_engine }
+  describe aws_rds_instance(input('rds_name')) do
+    its('engine') { should eq input('rds_engine') }
   end
 end
 
 control 'upgrades' do
-  impact 1.0
+  impact 0.5
+  tag nist: ['CM-6']
+  tag severity: 'medium'
   title 'auto upgrades'
-  describe aws_rds_instance(rds_name) do
-    its('auto_minor_version_upgrade')           { should eq rds_auto_minor_version_upgrade }
+  describe aws_rds_instance(input('rds_name')) do
+    its('auto_minor_version_upgrade') { should eq input('rds_auto_minor_version_upgrade') }
   end
 end
 
 control 'protection' do
-  impact 1.0
+  impact 0.5
+  tag nist: ['CM-6']
+  tag severity: 'medium'
   title 'deletion protection'
-  describe aws_rds_instance(rds_name) do
-    its('deletion_protection')                  { should eq rds_deletion_protection }
+  describe aws_rds_instance(input('rds_name')) do
+    its('deletion_protection') { should eq input('rds_deletion_protection') }
   end
 end
 
 control 'authentication' do
-  impact 1.0
+  impact 0.5
+  tag nist: ['CM-6']
+  tag severity: 'medium'
   title 'iam authentication'
-  describe aws_rds_instance(rds_name) do
-    its('iam_database_authentication_enabled')  { should eq rds_iam_database_authentication_enabled }
+  describe aws_rds_instance(input('rds_name')) do
+    its('iam_database_authentication_enabled') { should eq input('rds_iam_database_authentication_enabled') }
   end
 end
 
 control 'metrics' do
-  impact 1.0
+  impact 0.5
+  tag nist: ['CM-6']
+  tag severity: 'medium'
   title 'performance monitoring'
-  describe aws_rds_instance(rds_name) do
-    its('performance_insights_enabled')         { should eq rds_performance_insights_enabled }
+  describe aws_rds_instance(input('rds_name')) do
+    its('performance_insights_enabled') { should eq input('rds_performance_insights_enabled') }
   end
 end
 
 control 'tags' do
-  impact 1.0
-  title 'Has terraform created tags'
-  describe aws_rds_instance(rds_name) do
+  impact 0.5
+  tag nist: ['CM-6']
+  tag severity: 'medium'
+  title 'has terraform created tags'
+  describe aws_rds_instance(input('rds_name')) do
     its('tags') { should include('Terraform' => 'true') }
   end
 end
